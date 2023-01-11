f65b546fc78118a88c1655e1c4de6a488c5130a3
강수정 <kngsujng@naver.com>
2023-01-11 12:43:32 +0900

    Feat: 거스름돈 반환 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 const btnCokeItems = document.querySelectorAll(".btn_cokeItem");
 const btnDeposit = document.querySelector(".btn_deposit");
+const btnReturn = document.querySelector(".btn_return");
 const txtFund = document.querySelector(".fund");
 const txtBalance = document.querySelector(".balance");
 
 // 입금액을 input창에 입력하고 입금 버튼을 누르면
 // 1. 로컬스토리지에 저장
 // 2. 소지금에서 차감
-// 3. 잔액에 바로 반영
+// 3. 잔액 증가
 
 function deposit(event) {
   const inpDeposit = document.querySelector(".inp_deposit");
@@ -32,17 +33,43 @@ function deposit(event) {
     inpDeposit.value = "";
   }
 }
+btnDeposit.addEventListener("click", deposit);
 
+// 음료버튼 위에 마우스오버하면 active, 마우스리브하면 active 해제
 for (let i = 0; i < btnCokeItems.length; i++) {
   btnCokeItems[i].addEventListener("mouseenter", function () {
     btnCokeItems[i].classList.add("active");
+    console.log("들어왔다!");
   });
 }
-
 for (let i = 0; i < btnCokeItems.length; i++) {
   btnCokeItems[i].addEventListener("mouseleave", function () {
     btnCokeItems[i].classList.remove("active");
   });
 }
 
-btnDeposit.addEventListener("click", deposit);
+// btnCokeitem 누르면
+// 0. 잔액보다 적으면 alert창 띄워주기
+// 1. 잔액 감소
+// 2. preview 수정
+// 2-1. 이미 있으면 개수 바꿔주기
+// 2-2. 없으면 들어가기
+// 3. dummy데이터의 quantity가 0으로 되면 품절 띄워주기
+
+for (let i = 0; i < btnCokeItems.length; i++) {
+  btnCokeItems[i].addEventListener("click", function () {
+    if (+txtBalance.textContent < 1000) {
+      alert("잔액이 부족합니다😓 돈을 입금해주세요.");
+      return;
+    } else {
+      txtBalance.textContent = parseInt(txtBalance.textContent) - 1000;
+    }
+  });
+}
+
+// 거스름돈반환 버튼 누르면 : 잔액 0으로 감소
+function returnChange(e) {
+  e.preventDefault();
+  txtBalance.textContent = "";
+}
+btnReturn.addEventListener("click", returnChange);

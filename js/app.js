96bdded231cb64df57e472c0e3ab3ba7c2139928
강수정 <kngsujng@naver.com>
2023-01-26 00:41:46 +0900

    Feat: 획득 버튼 눌렀을 때 기능 구현

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@ const btnGet = document.querySelector(".btn_get");
 const txtBalance = document.querySelector(".balance");
 const txtFund = document.querySelector(".fund"); //5만원 소지금 고정
 const inpDeposit = document.querySelector(".inp_deposit");
-const ulSelected = document.querySelector(".Con-gotCokeList .gotCoke_list");
+const ulSelected = document.querySelector(
+  ".Con-gotCokeList_preview .gotCoke_list"
+);
+const ulGotcoke = document.querySelector(".Con-gotCokeList .gotCoke_list");
 
 // 입금액을 input창에 입력하고 입금 버튼을 누르면
 // 1. 로컬스토리지에 저장
@@ -159,5 +162,53 @@ function showSelectedItems() {
     ulSelected.appendChild(li);
   });
 }
-
 createCokeItems(state.coke);
+
+// 획득버튼 눌렀을 때
+function getUserCoke() {
+  // if (!state.balance || !state.selectedList.length) return;
+
+  state.selectedList.forEach((item) => {
+    const itemAlready = state.gotCoke.find((v) => v.id === item.id);
+
+    if (itemAlready) {
+      itemAlready.quantity += item.quantity;
+    } else {
+      state.gotCoke.push({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        img: item.img,
+        quantity: item.quantity,
+      });
+    }
+  });
+  showUserCoke();
+  return;
+}
+
+function showUserCoke() {
+  const ul = document.querySelector(".gotCoke_list");
+  ul.innerHTML = "";
+
+  state.gotCoke.forEach((item) => {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const gotCokeName = document.createElement("span");
+    const gotCokeCount = document.createElement("div");
+
+    li.classList.add("gotCoke_item");
+    gotCokeCount.classList.add("gotCoke_count");
+
+    img.src = `${item.img}`;
+    gotCokeName.textContent = item.name;
+    gotCokeCount.textContent = item.quantity;
+
+    li.append(img, gotCokeName, gotCokeCount);
+    ul.appendChild(li);
+    ulGotcoke.appendChild(ul);
+  });
+}
+
+btnGet.addEventListener("click", getUserCoke);
+btnGet.addEventListener("click", returnChange);
